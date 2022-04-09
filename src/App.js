4ecf6faaf9cc756aacd9bb704e32fe8b1f73d630
Vahid Aghayev <aghayevvahid1@gmail.com>
2022-04-09 22:39:49 +0400

    Front-end-mentor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiceFive } from '@fortawesome/free-solid-svg-icons';
 
+
+
+import "./style.css";
 function App() {
+
+const [alma,setAlma] = useState(true);
+const [quote,setQuote] = useState({id:[],advice:[]}) 
+
+  useEffect(()=>{
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(data => setQuote({id:data.slip.id,advice:data.slip.advice}));
+    
+},[alma])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="body">
+     
+      <div className="quoteDiv">
+
+        <h3 className="quoteH">Advice #{quote.id}</h3>
+        <p className="quoteP">"{quote.advice}"</p>
+        <button className="changeButton" onClick={()=>{setAlma(!alma)}}>  <FontAwesomeIcon className="icon" icon={faDiceFive}/></button>
+
+      </div>
+
     </div>
+
   );
 }
 
 export default App;
+
